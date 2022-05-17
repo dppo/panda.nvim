ad77ff611ece1a2f98f9452a95627e14061b2883
“dppo” <xiaogao233@163.com>
2022-05-17 21:26:06 +0800

    fzf功能优化

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,3 +1,15 @@
+vim.cmd [[
+function! RipgrepFzf(query, fullscreen)
+  let command_fmt = 'rg --column --line-number --no-heading --color=always --smart-case -- %s || true'
+  let initial_command = printf(command_fmt, shellescape(a:query))
+  let reload_command = printf(command_fmt, '{q}')
+  let spec = {'options': ['--phony', '--query', a:query, '--bind', 'change:reload:'.reload_command]}
+  call fzf#vim#grep(initial_command, 1, fzf#vim#with_preview(spec), a:fullscreen)
+endfunction
+
+command! -nargs=* -bang RG call RipgrepFzf(<q-args>, <bang>0)
+]]
+
 --Remap space as leader key
 vim.keymap.set({"n", "v"}, "<Space>", "<Nop>", {silent = true})
 vim.g.mapleader = " "
@@ -18,12 +30,29 @@ vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns reset_hunk<CR>")
 
 --fzf
 vim.keymap.set("n", "<leader>ff", "<cmd>Files<CR>")
-vim.keymap.set("n", "<leader>fw", "<cmd>Rg<CR>")
-vim.keymap.set("x", "<leader>fw", 'y:Rg <C-R>"<CR>')
+vim.keymap.set("n", "<leader>fa", "<cmd>RG<CR>")
 vim.keymap.set("n", "<leader>fb", "<cmd>Buffers<CR>")
 vim.keymap.set("n", "<leader>fh", "<cmd>History<CR>")
 vim.keymap.set("n", "<leader>fg", "<cmd>GFiles?<CR>")
+vim.keymap.set(
+  "v",
+  "<leader>fa",
+  function()
+    vim.cmd('noau normal! "vy"')
+    local content = vim.fn.getreg("v")
+    vim.api.nvim_command("RG " .. content)
+  end
+)
+vim.keymap.set(
+  "n",
+  "<leader>fw",
+  function()
+    local current_word = vim.call("expand", "<cword>")
+    vim.api.nvim_command("RG " .. current_word)
+  end
+)
 
+-- copy to system clipboard
 vim.keymap.set(
   "v",
   "<leader>y",
